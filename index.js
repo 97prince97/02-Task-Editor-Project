62d44730e8688e31bda4f29905f89b80c57e2c8a
97prince97 <97prince97@gmail.com>
2021-06-28 11:58:11 +0530

    +feature contentEditable card

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const generateNewCard = (taskData) =>
   <div class="col-md-6 col-lg-4">
   <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
-      <button type="button" class="btn btn-outline-success">
-        <i class="fas fa-pencil-alt"></i>
+      <button type="button" class="btn btn-outline-success" id=${taskData.id}  onclick="editCard.apply(this,arguments)">
+        <i class="fas fa-pencil-alt" id=${taskData.id} onclick="editCard.apply(this,arguments)"></i>
       </button>
       <button type="button" class="btn btn-outline-danger"  id=${taskData.id} onclick="deleteCard.apply(this,arguments)">
         <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i>
@@ -21,14 +21,14 @@ const generateNewCard = (taskData) =>
       style="width: 18rem"
     />
     <div class="card-body">
-      <h5 class="card-title">${taskData.taskTitle}</h5>
-      <p class="card-text">
+      <h5 class="card-title" contenteditable="false" id="edit">${taskData.taskTitle}</h5>
+      <p class="card-text" contenteditable="false" id="edit">
       ${taskData.taskDescription}
       </p>
-      <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+      <a href="#" class="btn btn-primary" contenteditable="false" id="edit">${taskData.taskType}</a>
     </div>
     <div class="card-footer">
-      <button type="button" class="btn btn-outline-primary float-end">
+      <button type="button" class="btn btn-outline-primary float-end btn__text__change">
         Open Task
       </button>
     </div>
@@ -80,7 +80,7 @@ const deleteCard = (event) => {
   //if match found remove
 
   globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
-  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore })); //an object
 
   //contact parent
   if (tagname === "BUTTON") {
@@ -92,4 +92,32 @@ const deleteCard = (event) => {
       event.target.parentNode.parentNode.parentNode.parentNode
     );
   }
+};
+
+//on click pencil
+//access task_title, task_type, task_description
+//set contenteditable attribute value to true in all these
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  let parentElement;
+
+  if (tagname === "button") {
+    parentElement = event.target.parentNode.parentNode;
+  } else {
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+
+  submitButton.innerText = "Save Changes";
 };
